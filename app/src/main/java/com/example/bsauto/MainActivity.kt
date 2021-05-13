feafe00a1d9a5a9efdec903c86f62f228cf7e0b0
AlmudenaRA <almudena.ruedas@gmail.com>
2021-05-13 15:50:47 +0200

    Editar o eliminar usuario

--- app/src/main/java/com/example/bsauto/MainActivity.kt
+++ app/src/main/java/com/example/bsauto/MainActivity.kt
@@ -3,6 +3,17 @@ package com.example.bsauto
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -10,6 +21,14 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    companion object{
+        lateinit var img_user: ImageView
+        lateinit var txt_header_user: TextView
+        lateinit var txt_header_email: TextView
+    }
+
 
     enum class ProviderType{
         BASIC,
@@ -20,10 +39,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_start, R.id.nav_my_post, R.id.nav_map,
+            R.id.nav_profile, R.id.nav_exit), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
         //Guardar datos del login del usuario autenticado
@@ -31,6 +63,12 @@ class MainActivity : AppCompatActivity() {
         prefs.putString("email", email)
         prefs.putString("provider", provider)
         prefs.apply()
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     //Mostrar el email y contraseña
@@ -45,4 +83,6 @@ class MainActivity : AppCompatActivity() {
             //onBackPressed()//vuelve a la pantalla anterior
         //}
     }
+
+
 }
